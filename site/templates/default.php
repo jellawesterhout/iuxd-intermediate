6b1d3032cb3b5569f4533d502c8f8c226756f7ed
Jella <jellawesterhout@gmail.com>
2019-11-23 18:34:08 +0100

    Wie neemt deel...

--- site/templates/default.php
+++ site/templates/default.php
@@ -60,108 +60,152 @@
       </div>
     </div>
 
-      <div class="hero">
-        <div class="container margin flex">
-          <div class="hero-left margin">
-            <div class="col">
-              <h5><?= $page->title_verloop() ?></h5>
-            </div>
+    <div class="container margin">
+      <div class="row justify-content-md-center">
+        <div class="col-7">
+          <h5><?= $page->title_wie() ?></h5>
+          <p><?= $page->wie() ?></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container margin">
+      <div class="row justify-content-md-center">
+        <div class="col-4 flex justify-content-around">
+          <div class="col">
+            <img src="assets/images/wie1.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie1() ?></h6>
           </div>
-          <div class="hero-right margin">
-            <div class="col-md-12 offset-md-1">
-              <!--Accordion wrapper-->
-              <div class="accordion md-accordion accordion-4" id="accordionEx2" role="tablist" aria-multiselectable="true">
-
-              <!-- Accordion card -->
-              <div class="card rounded shadow-sm">
-
-                <!-- Card header -->
-                <div class="card-header z-depth-1" role="tab" id="heading10">
-                  <h6 id="underline1"><span class="align-middle"><a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse10" aria-expanded="true"
-                  aria-controls="collapse10"><?= $page->maandag () ?></a></span></h6>
-                </div>
-                <!-- Card body -->
-                <div id="collapse10" class="collapse show" role="tabpanel" aria-labelledby="heading10"
-                  data-parent="#accordionEx2">
-                  <div class="card-body rgba-teal-strong white-text">
-                    <p><?= $page->maandag_description () ?></p>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Accordion card -->
-              <div class="card rounded shadow-sm">
-
-                <!-- Card header -->
-                <div class="card-header z-depth-1" role="tab" id="heading11">
-                  <h6 id="underline2"><span class="align-middle"><a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse11" aria-expanded="false"
-                  aria-controls="collapse11"><?= $page->dinsdag () ?></a></span></h6>
-                </div>
-                <!-- Card body -->
-                <div id="collapse11" class="collapse" role="tabpanel" aria-labelledby="heading11"
-                  data-parent="#accordionEx2">
-                  <div class="card-body rgba-teal-strong white-text">
-                    <p><?= $page->dinsdag_description () ?></p>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Accordion card -->
-              <div class="card rounded shadow-sm">
-
-                <!-- Card header -->
-                <div class="card-header z-depth-1" role="tab" id="heading12">
-                  <h6 id="underline3"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse12" aria-expanded="false"
-                  aria-controls="collapse12"><?= $page->woensdag () ?></a></span></h6>
-                </div>
-                <!-- Card body -->
-                <div id="collapse12" class="collapse" role="tabpanel" aria-labelledby="heading12"
-                  data-parent="#accordionEx2">
-                  <div class="card-body rgba-teal-strong white-text">
-                    <p><?= $page->woensdag_description () ?></p>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Accordion card -->
-              <div class="card rounded shadow-sm">
-
-                <!-- Card header -->
-                <div class="card-header z-depth-1" role="tab" id="heading13">
-                  <h6 id="underline4"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse13" aria-expanded="false"
-                  aria-controls="collapse13"><?= $page->donderdag () ?></a></span></h6>
-                </div>
-                <!-- Card body -->
-                <div id="collapse13" class="collapse" role="tabpanel" aria-labelledby="heading13"
-                  data-parent="#accordionEx2">
-                  <div class="card-body rgba-teal-strong white-text">
-                    <p><?= $page->donderdag_description () ?></p>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Accordion card -->
-              <div class="card rounded shadow-sm">
-
-                <!-- Card header -->
-                <div class="card-header z-depth-1" role="tab" id="heading14">
-                  <h6 id="underline5"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse14" aria-expanded="false"
-                  aria-controls="collapse14"><?= $page->vrijdag () ?></a></span></h6>
-                </div>
-                <!-- Card body -->
-                <div id="collapse14" class="collapse" role="tabpanel" aria-labelledby="heading14"
-                  data-parent="#accordionEx2">
-                  <div class="card-body rgba-teal-strong white-text">
-                    <p><?= $page->vrijdag_description () ?></p>
-                  </div>
-                </div>
-              </div>
-              </div>
-              <!--/.Accordion wrapper-->
-            </div>
+          <div class="col">
+            <img src="assets/images/wie2.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie2() ?></h6>
+          </div>
+        </div>
+        <div class="col-6 flex justify-content-around">
+          <div class="col">
+            <img src="assets/images/wie3.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie3() ?></h6>
+          </div>
+          <div class="col">
+            <img src="assets/images/wie4.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie4() ?></h6>
+          </div>
+          <div class="col">
+            <img src="assets/images/wie5.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie5() ?></h6>
+          </div>
+        </div>
+        <div class="col-2 flex justify-content-around">
+          <div class="col">
+            <img src="assets/images/wie6.png" style="height: 50%; margin-bottom: 15px;"/>
+            <h6 class="wie_description text-center"><?= $page->wie6() ?></h6>
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="hero">
+      <div class="container margin flex">
+        <div class="hero-left margin">
+          <div class="col">
+            <h5><?= $page->title_verloop() ?></h5>
+          </div>
+        </div>
+        <div class="hero-right margin">
+          <div class="col-md-12 offset-md-1">
+            <!--Accordion wrapper-->
+            <div class="accordion md-accordion accordion-4" id="accordionEx2" role="tablist" aria-multiselectable="true">
+
+            <!-- Accordion card -->
+            <div class="card rounded shadow-sm">
+
+              <!-- Card header -->
+              <div class="card-header z-depth-1" role="tab" id="heading10">
+                <h6 id="underline1"><span class="align-middle"><a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse10" aria-expanded="true"
+                aria-controls="collapse10"><?= $page->maandag () ?></a></span></h6>
+              </div>
+              <!-- Card body -->
+              <div id="collapse10" class="collapse show" role="tabpanel" aria-labelledby="heading10"
+                data-parent="#accordionEx2">
+                <div class="card-body rgba-teal-strong white-text">
+                  <p><?= $page->maandag_description () ?></p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion card -->
+            <div class="card rounded shadow-sm">
+
+              <!-- Card header -->
+              <div class="card-header z-depth-1" role="tab" id="heading11">
+                <h6 id="underline2"><span class="align-middle"><a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse11" aria-expanded="false"
+                aria-controls="collapse11"><?= $page->dinsdag () ?></a></span></h6>
+              </div>
+              <!-- Card body -->
+              <div id="collapse11" class="collapse" role="tabpanel" aria-labelledby="heading11"
+                data-parent="#accordionEx2">
+                <div class="card-body rgba-teal-strong white-text">
+                  <p><?= $page->dinsdag_description () ?></p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion card -->
+            <div class="card rounded shadow-sm">
+
+              <!-- Card header -->
+              <div class="card-header z-depth-1" role="tab" id="heading12">
+                <h6 id="underline3"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse12" aria-expanded="false"
+                aria-controls="collapse12"><?= $page->woensdag () ?></a></span></h6>
+              </div>
+              <!-- Card body -->
+              <div id="collapse12" class="collapse" role="tabpanel" aria-labelledby="heading12"
+                data-parent="#accordionEx2">
+                <div class="card-body rgba-teal-strong white-text">
+                  <p><?= $page->woensdag_description () ?></p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion card -->
+            <div class="card rounded shadow-sm">
+
+              <!-- Card header -->
+              <div class="card-header z-depth-1" role="tab" id="heading13">
+                <h6 id="underline4"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse13" aria-expanded="false"
+                aria-controls="collapse13"><?= $page->donderdag () ?></a></span></h6>
+              </div>
+              <!-- Card body -->
+              <div id="collapse13" class="collapse" role="tabpanel" aria-labelledby="heading13"
+                data-parent="#accordionEx2">
+                <div class="card-body rgba-teal-strong white-text">
+                  <p><?= $page->donderdag_description () ?></p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion card -->
+            <div class="card rounded shadow-sm">
+
+              <!-- Card header -->
+              <div class="card-header z-depth-1" role="tab" id="heading14">
+                <h6 id="underline5"><span class="align-middle"><a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse14" aria-expanded="false"
+                aria-controls="collapse14"><?= $page->vrijdag () ?></a></span></h6>
+              </div>
+              <!-- Card body -->
+              <div id="collapse14" class="collapse" role="tabpanel" aria-labelledby="heading14"
+                data-parent="#accordionEx2">
+                <div class="card-body rgba-teal-strong white-text">
+                  <p><?= $page->vrijdag_description () ?></p>
+                </div>
+              </div>
+            </div>
+            </div>
+            <!--/.Accordion wrapper-->
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
